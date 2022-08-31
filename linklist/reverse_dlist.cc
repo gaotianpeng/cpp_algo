d345cbd5f9af3f01444486019cd0b33bda66573a
gaotianpeng <gtpsydm@gmail.com>
2022-09-01 07:11:52 +0800

    daily practise

--- linklist/reverse_dlist.cc
+++ linklist/reverse_dlist.cc
@@ -13,8 +13,8 @@ ListDNode* ReverseDoubleList(ListDNode* head) {
 
 	ListDNode* prev = nullptr;
 	ListDNode* next = nullptr;
+
 	while (head != nullptr) {
-		// 保存下一结点
 		next = head->next;
 		head->next = prev;
 		head->prev = next;
@@ -25,29 +25,30 @@ ListDNode* ReverseDoubleList(ListDNode* head) {
 	return prev;
 }
 
-TEST(LinkListTest, ReverseDoubleList) {
-	cout << "reverse double list test start\n";
-	int test_times = 500000;
-	int max_n = 100;
-	int min_val = -20;
-	int max_val = 30;
-
-	for (int i = 0; i < test_times; i++) {
-		ListDNode* head = GenRandomDList(max_n, min_val, max_val);
-		ListDNode* copy = CopyDList(head);
-		ListDNode* reverse = ReverseDoubleList(head);
-		if (!IsReverse(copy, reverse)) {
-			PrintDList(copy);
-			PrintDList(reverse);
-			FreeList(copy);
-			FreeList(reverse);
-			EXPECT_TRUE(false);
-			break;
-		}
-		FreeList(copy);
-		FreeList(reverse);
-	}
-
-	cout << "test success\n";
-	cout << "reverse double list test end\n\n";
-}
+//
+//TEST(LinkListTest, ReverseDoubleList) {
+//	cout << "reverse double list test start\n";
+//	int test_times = 500000;
+//	int max_n = 100;
+//	int min_val = -20;
+//	int max_val = 30;
+//
+//	for (int i = 0; i < test_times; i++) {
+//		ListDNode* head = GenRandomDList(max_n, min_val, max_val);
+//		ListDNode* copy = CopyDList(head);
+//		ListDNode* reverse = ReverseDoubleList(head);
+//		if (!IsReverse(copy, reverse)) {
+//			PrintDList(copy);
+//			PrintDList(reverse);
+//			FreeList(copy);
+//			FreeList(reverse);
+//			EXPECT_TRUE(false);
+//			break;
+//		}
+//		FreeList(copy);
+//		FreeList(reverse);
+//	}
+//
+//	cout << "test success\n";
+//	cout << "reverse double list test end\n\n";
+//}
