48683f19a726701703b8e0d47d10b92a1fc011a5
gaotianpeng <gtpsydm@gmail.com>
2022-12-26 15:12:33 +0800

    daily practise

--- sort/bigger_than_twice.cc
+++ sort/bigger_than_twice.cc
@@ -18,98 +18,111 @@ using namespace tools;
 		2的后面没有
 		所以总共有5个
  */
+
 static int merge(vector<int>& arr, int left, int mid, int right) {
-	int ans = 0;
-	int win_r = mid + 1;
-	for (int i = left; i <= mid; ++i) {
-		while (win_r <= right && (long)arr[i] > (long)arr[win_r]*2) {
-			++win_r;
-		}
-		ans += win_r - mid - 1;
-	}
+    int win_r = mid + 1;
+    int ans = 0;
+    for (int i = left; i <= mid; ++i) {
+        while (win_r <= right && (long)arr[i] > (long)(2*arr[win_r])) {
+            win_r++;
+        }
 
-	int n = right - left + 1;
-	int *helper = new int[n];
-	int i = 0;
-	int p1 = left;
-	int p2 = mid + 1;
-	while (p1 <= mid && p2 <= right) {
-		helper[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
-	}
+        ans += right - win_r + 1;
+    }
 
-	while (p1 <= mid) {
-		helper[i++] = arr[p1++];
-	}
+    int n = right - left + 1;
+    int * helper = new int[n];
+    int i = 0;
+    int p1 = left;
+    int p2 = mid + 1;
+    while (p1 <= mid && p2 <= right) {
+        helper[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+    }
 
-	while(p2 <= right) {
-		helper[i++] = arr[p2++];
-	}
+    while (p1 <= mid) {
+        helper[i++] = arr[p1++];
+    }
 
-	for (int i = 0; i < n; ++i) {
-		arr[left + i] = helper[i];
-	}
+    while (p2 <= right) {
+        helper[i++] = arr[p2++];
+    }
 
-	delete [] helper;
+    for (int i = 0; i < n; ++i) {
+        arr[left + i] = helper[i];
+    }
 
-	return ans;
+    return ans;
 }
 
-static int process(vector<int>& arr, int left, int right) {
-	if (left == right) {
-		return 0;
-	}
-
-	int mid = left + ((right - left) >> 1);
-	return process(arr, left, mid)
-		+ process(arr, mid + 1, right)
-		+ merge(arr, left, mid, right);
-}
 
 static int BiggerThanTwice(vector<int> arr) {
-	if (arr.size() < 2) {
-		return 0;
-	}
+    if (arr.size() < 2) {
+        return 0;
+    }
 
-	return process(arr, 0, arr.size() - 1);
+    int merge_size = 1;
+    int n = arr.size();
+    int ans = 0;
+    while (merge_size < n) {
+        int left = 0;
+        while (left < n) {
+            if (merge_size > n - left) {
+                break;
+            }
+
+            int mid = left + merge_size - 1;
+            int right = mid + std::min(merge_size, n - mid - 1);
+            ans += merge(arr, left, mid, right);
+            left = right + 1;
+        }
+
+        if (merge_size > n / 2) {
+            break;
+        }
+
+        merge_size <<= 1;
+    }
+
+    return ans;
 }
 
 static int test(vector<int>& arr) {
-	if (arr.size() < 2) {
-		return 0;
-	}
+    if (arr.size() < 2) {
+        return 0;
+    }
 
-	int ans = 0;
-	int n = arr.size();
-	for (int i = 0; i < n - 1; ++i) {
-		for (int j = i + 1; j < n; ++j) {
-			if ((long)arr[i] > ((long)arr[j] << 1)) {
-				++ans;
-			}
-		}
-	}
+    int ans = 0;
+    int n = arr.size();
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if ((long)arr[i] > ((long)arr[j] << 1)) {
+                ++ans;
+            }
+        }
+    }
 
-	return ans;
+    return ans;
 }
 
 //TEST(SortTest, BiggerThanTwiceTest) {
-//	cout << "bigger than twice test start\n";
-//	int max_n = 100;
-//	int min_val = numeric_limits<int>::min();
-//	int max_val = numeric_limits<int>::max();
-//	int test_times = 500000;
-//	for (int i = 0; i < test_times; i++) {
-//		vector<int> arr1;
-//		vector<int> arr2;
-//		RandomArr(arr1, max_n, min_val, max_val);
-//		CopyArr(arr1, arr2);
-//		int ans1 = test(arr1);
-//		int ans2 = BiggerThanTwice(arr2);
-//		if (ans1 != ans2) {
-//			cout << ans1 << endl;
-//			cout << ans2 << endl;
-//			ASSERT_TRUE(false);
-//		}
-//	}
-//	cout << "test success\n";
-//	cout << "bigger than twice test end\n\n";
+//    cout << "bigger than twice test start\n";
+//    int max_n = 100;
+//    int min_val = numeric_limits<int>::min();
+//    int max_val = numeric_limits<int>::max();
+//    int test_times = 500000;
+//    for (int i = 0; i < test_times; i++) {
+//        vector<int> arr1;
+//        vector<int> arr2;
+//        RandomArr(arr1, max_n, min_val, max_val);
+//        CopyArr(arr1, arr2);
+//        int ans1 = test(arr1);
+//        int ans2 = BiggerThanTwice(arr2);
+//        if (ans1 != ans2) {
+//            cout << ans1 << endl;
+//            cout << ans2 << endl;
+//            ASSERT_TRUE(false);
+//        }
+//    }
+//    cout << "test success\n";
+//    cout << "bigger than twice test end\n\n";
 //}
