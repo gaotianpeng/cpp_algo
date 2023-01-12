0997e3721c1247a19fcd2d77e8a10d58566580cc
gaotianpeng <gtpsydm@gmail.com>
2023-01-12 23:09:37 +0800

    daily practise

--- linklist/is_palindrome_list.cc
+++ linklist/is_palindrome_list.cc
@@ -7,7 +7,60 @@
 using namespace std;
 using namespace tools;
 
+static ListNode* reverseListNode(ListNode* head) {
+    if (head == nullptr) {
+        return nullptr;
+    }
+
+    ListNode* pre = nullptr;
+    ListNode* next = nullptr;
+    while (head != nullptr) {
+        next = head->next;
+        head->next = pre;
+        pre = head;
+        head = next;
+    }
+
+    return pre;
+}
+
 static bool IsPalindromeList(ListNode* head) {
+    if (head == nullptr || head->next == nullptr) {
+        return true;
+    }
+
+    ListNode* slow = head;
+    ListNode* fast = head;
+    while (fast->next != nullptr && fast->next->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    ListNode* reverse_head = slow->next;
+    slow->next = nullptr;
+
+    reverse_head = reverseListNode(reverse_head);
+    ListNode* from_head = head;
+    ListNode* from_end = reverse_head;
+
+    bool ret = true;
+    while (from_head != nullptr && from_end != nullptr) {
+        if (from_head->val != from_end->val) {
+            ret = false;
+            break;
+        }
+        from_head = from_head->next;
+        from_end = from_end->next;
+    }
+
+    ListNode* back_half_head = reverseListNode(reverse_head);
+    slow->next = back_half_head;
+
+    return ret;
+}
+
+
+static bool IsPalindromeList1(ListNode* head) {
     if (head == nullptr || head->next == nullptr) {
         return true;
     }
@@ -26,13 +79,12 @@ static bool IsPalindromeList(ListNode* head) {
     }
 
     while (!nodes.empty()) {
-        if (head->val != nodes.top()->val) {
+        if (nodes.top()->val != head->val) {
             return false;
         }
         nodes.pop();
         head = head->next;
     }
-
 
     return true;
 }
@@ -61,10 +113,10 @@ static bool IsPalindromeListTest(ListNode* head) {
     return true;
 }
 
-//
+
 //TEST(LinkListTest, IsPalindromeListTest) {
 //	cout << "is palindrome list test start\n";
-//	int test_times = 500000;
+//	int test_times = 100000;
 //	int max_n = 100;
 //	int min_val = -20;
 //	int max_val = 30;
@@ -72,6 +124,14 @@ static bool IsPalindromeListTest(ListNode* head) {
 //	for (int i = 0; i < test_times; i++) {
 //		ListNode* head = GenRandomList(max_n, min_val, max_val);
 //        if (IsPalindromeList(head) != IsPalindromeListTest(head)) {
+//            EXPECT_TRUE(false);
+//        }
+//		FreeList(head);
+//	}
+//
+//	for (int i = 0; i < test_times; i++) {
+//		ListNode* head = GenRandomList(max_n, min_val, max_val);
+//        if (IsPalindromeList1(head) != IsPalindromeListTest(head)) {
 //            EXPECT_TRUE(false);
 //        }
 //		FreeList(head);
