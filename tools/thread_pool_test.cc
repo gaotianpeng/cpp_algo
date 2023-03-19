b685157e889182222f139d158540fe25ad07f07d
gaotianpeng <gtpsydm@gmail.com>
2023-03-19 16:09:11 +0800

    daily practise

--- tools/thread_pool_test.cc
+++ tools/thread_pool_test.cc
@@ -5,6 +5,7 @@
 #include "gtest/gtest.h"
 #include <iostream>
 #include <chrono>
+#include <set>
 #include "thread_pool.h"
 
 using namespace std;
@@ -21,14 +22,36 @@ struct Foo {
     int data = 10;
 };
 
-//TEST(ThreadPool, ThreadPoolTest) {
-//    ThreadPool thread_pool(4);
-//    thread_pool.Start();
-//    thread_pool.Run(Func, 3);
-//
-//    Foo foo;
-//    auto f = std::bind(&Foo::print_sum, &foo, std::placeholders::_1, std::placeholders::_1);
-//    thread_pool.Run(f, 3, 4);
-//    std::this_thread::sleep_for(std::chrono::seconds(1));
-//    thread_pool.Stop();
-//}
+void sampleFunction() {
+    cout << "endl" << endl;
+    throw std::runtime_error("An error occurred.");
+}
+
+std::thread::id PrintThreadId() {
+    return std::this_thread::get_id();
+}
+
+
+TEST(ThreadPool, ThreadPoolTest) {
+    ThreadPool pool;
+    pool.Start();
+    pool.Run(&sampleFunction);
+    std::this_thread::sleep_for(std::chrono::seconds (5));
+
+    int i = 10000;
+    std::set<std::thread::id> idsets;
+    std::vector<std::shared_ptr<std::future<std::thread::id>>> vecs;
+
+    while(i >= 0) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::shared_ptr<std::future<std::thread::id>> id = pool.Run(&PrintThreadId);
+        vecs.push_back(id);
+    }
+
+    for (int j = 0; j < vecs.size(); ++j) {
+        auto id = vecs[j]->get();
+        idsets.insert(id);
+    }
+
+    cout << idsets.size() <<endl;
+}
