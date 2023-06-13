61396e167425de92f8b4bce698df90b03c609241
gaotianpeng <gtpsydm@gmail.com>
2023-06-13 14:01:53 +0000

    daily practise

---
+++ leetcode/linklist/0234_palindrome_linked_list.cc
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <random>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+/*
+    for test
+*/
+namespace {
+
+class Math {
+public:
+    static double random() {
+        static std::random_device rd;
+        static std::default_random_engine engine(rd());
+        static std::uniform_real_distribution<> distribution(0.0, 1.0);
+        return distribution(engine);
+    }
+};
+
+static void RandomArr(vector<int>& out, int max_n, int min_val, int max_val) {
+	int len = (int)(Math::random() * max_n);
+	for (int i = 0; i < len; i++) {
+		int val = (int)(Math::random() * (max_val - min_val + 1)) + min_val;
+		out.emplace_back(val);
+	}
+}
+
+struct ListNode{
+    int val  = 0;
+    ListNode* next = nullptr;
+
+    ListNode(int v):val(v) {
+    }
+};
+
+static ListNode* RandomSortedList(int max_n, int min_val, int max_val) {
+    vector<int> out;
+    RandomArr(out, max_n, min_val, max_val);
+    std::sort(out.begin(), out.end());
+    vector<ListNode*> lists;
+    for (auto& elem: out) {
+        lists.emplace_back(new ListNode(elem));
+    }
+
+    if (lists.size() == 0) {
+        return nullptr;
+    }
+
+    for (int i = 1; i < lists.size(); ++i) {
+        lists[i-1]->next = lists[i];
+    }
+    lists[lists.size() - 1]->next = nullptr;
+    return lists[0];
+}
+
+static ListNode* CopyList(ListNode* head) {
+    if (head == nullptr) {
+        return nullptr;
+    }
+
+    ListNode* ans = new ListNode(head->val);
+    ListNode* ans_head = ans;
+    while (head->next != nullptr) {
+        ans->next = new ListNode(head->next->val);
+        ans = ans->next;
+        head = head->next;
+    }
+    return ans_head;
+}
+
+static void PrintList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+static bool IsEqual(ListNode* head1, ListNode* head2) {
+    while (head1 != nullptr && head2 != nullptr) {
+        if (head1->val != head2->val) {
+            return false;
+        }
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+
+    if (head1 != nullptr || head2 != nullptr) {
+        return false;
+    }
+    return true;
+}
+
+static void FreeList(ListNode* head) {
+    while (head != nullptr) {
+        ListNode* tmp = head;
+        head = head->next;
+        delete tmp;
+    }
+}
+
+} // namespace
+
+/*
+    https://leetcode.cn/problems/palindrome-linked-list/
+    0234 回文链表
+        给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 
+*/
+
+bool isPalindrome(ListNode* head) {
+    return true;
+}
+
+bool test(ListNode* head) {
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    cout << "test start..." << endl;
+    int max = 100;
+    int min = -100;
+    int max_n = 10;
+    int test_times = 100000;
+
+    for (int i = 0; i < test_times; ++i) {
+        ListNode* head = RandomSortedList(max_n, min, max);
+        if (isPalindrome(head) != test(head)) {
+            cout << "test failed" << endl;
+            FreeList(head);
+            break;
+        }
+
+        FreeList(head);
+    }
+
+    cout << "test end" << endl;
+
+    return 0;
+}
