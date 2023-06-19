2092d4cd522f3d19573484016dbca05cdfc363a1
gaotianpeng <gtpsydm@gmail.com>
2023-06-19 12:23:32 +0000

    daily practise

---
+++ leetcode/linklist/2095_delete_the_middle_node_of_linked_list.cc
@@ -0,0 +1,205 @@
+#include <iostream>
+#include <random>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/*
+    for test
+*/
+namespace {
+
+class Math {
+public:
+    static double random() {
+        static std::random_device rd;
+        static std::default_random_engine engine(rd());
+        static std::uniform_real_distribution<> distribution(0.0, 1.0);
+        return distribution(engine);
+    }
+};
+
+static void RandomArr(vector<int>& out, int max_n, int min_val, int max_val) {
+	int len = (int)(Math::random() * max_n);
+	for (int i = 0; i < len; i++) {
+		int val = (int)(Math::random() * (max_val - min_val + 1)) + min_val;
+		out.emplace_back(val);
+	}
+}
+
+struct ListNode{
+    int val  = 0;
+    ListNode* next = nullptr;
+
+    ListNode(int v):val(v) {
+    }
+};
+
+static ListNode* RandomList(int max_n, int min_val, int max_val) {
+    vector<int> out;
+    RandomArr(out, max_n, min_val, max_val);
+    vector<ListNode*> lists;
+    for (auto& elem: out) {
+        lists.emplace_back(new ListNode(elem));
+    }
+
+    if (lists.size() == 0) {
+        return nullptr;
+    }
+
+    for (int i = 1; i < lists.size(); ++i) {
+        lists[i-1]->next = lists[i];
+    }
+    lists[lists.size() - 1]->next = nullptr;
+    return lists[0];
+}
+
+static ListNode* CopyList(ListNode* head) {
+    if (head == nullptr) {
+        return nullptr;
+    }
+
+    ListNode* ans = new ListNode(head->val);
+    ListNode* ans_head = ans;
+    while (head->next != nullptr) {
+        ans->next = new ListNode(head->next->val);
+        ans = ans->next;
+        head = head->next;
+    }
+    return ans_head;
+}
+
+static void PrintList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+static bool IsEqual(ListNode* head1, ListNode* head2) {
+    while (head1 != nullptr && head2 != nullptr) {
+        if (head1->val != head2->val) {
+            return false;
+        }
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+
+    if (head1 != nullptr || head2 != nullptr) {
+        return false;
+    }
+    return true;
+}
+
+static void FreeList(ListNode* head) {
+    while (head != nullptr) {
+        ListNode* tmp = head;
+        head = head->next;
+        delete tmp;
+    }
+}
+
+} // namespace
+
+/*
+    https://leetcode.cn/problems/delete-the-middle-node-of-a-linked-list/
+    2095. 删除链表的中间节点
+        给你一个链表的头节点 head 。删除 链表的 中间节点 ，并返回修改后的链表的头节点 head 。
+        长度为 n 链表的中间节点是从头数起第 ⌊n / 2⌋ 个节点（下标从 0 开始），其中 ⌊x⌋ 表示小于或等于 x 的最大整数。
+        对于 n = 1、2、3、4 和 5 的情况，中间节点的下标分别是 0、1、1、2 和 2 。
+
+*/
+static ListNode* deleteMiddle(ListNode* head) {
+    if (head == nullptr) {
+        return nullptr;
+    }
+
+    if (head->next == nullptr) {
+        delete head;
+        return nullptr;
+    }
+    
+    ListNode* slow = head;
+    ListNode* fast = head;
+    ListNode* pre = nullptr;
+    while (fast && fast->next) {
+        fast = fast->next->next;
+        pre = slow;
+        slow = slow->next;
+    }
+    ListNode* next = pre->next;
+    pre->next = pre->next->next;
+    delete next;
+    return head;
+}
+
+static ListNode* test(ListNode* head) {
+    if (head == nullptr) {
+        return nullptr;
+    }
+
+    if (head->next == nullptr) {
+        delete head;
+        return nullptr;
+    }
+
+    if (head->next->next == nullptr) {
+        delete head->next;
+        head->next = nullptr;
+        return head;
+    }
+
+    if (head->next->next->next == nullptr) {
+        ListNode* to_delete = head->next;
+        head->next = head->next->next;
+        delete to_delete;
+        return head;
+    }
+
+
+    vector<ListNode*> nodes;
+    while (head != nullptr) {
+        nodes.emplace_back(head);
+        head = head->next;
+    }
+
+    int n = nodes.size();
+    int node_to_delete = n / 2;
+    nodes[node_to_delete - 1]->next = nodes[node_to_delete + 1];
+    delete nodes[node_to_delete];
+
+    return nodes[0];
+}
+
+
+int main(int argc, char* argv[]) {
+    cout << "test start..." << endl;
+    int max = 10000;
+    int min = -10000;
+    int max_n = 20;
+    int test_times = 50000;
+
+    for (int i = 0; i < test_times; ++i) {
+        ListNode* head1 = RandomList(max_n, min, max);
+        ListNode* head2 = CopyList(head1);
+
+        ListNode* list1 = deleteMiddle(head1);
+        ListNode* list2 = test(head2);
+        if (!IsEqual(list1, list2)) {
+            cout << "test failed" << endl;
+            PrintList(list1);
+            PrintList(list2);
+            FreeList(list1);
+            FreeList(list2);
+            break;
+        }
+
+        FreeList(list1);
+        FreeList(list2);
+    }
+
+    cout << "test end" << endl;
+
+    return 0;
+}
