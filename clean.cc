7c9900c2d059412a5373599de38f180f53627799
gaotianpeng <gtpsydm@gmail.com>
2023-06-21 06:49:38 +0000

    daily practise

--- clean.cc
+++ clean.cc
@@ -6,28 +6,6 @@
 using namespace std;
 namespace fs = std::filesystem;
 
-static bool is_need_clean(std::string file_name) {
-    if (file_name.size() < 3) {
-        return false;
-    }
-
-    if (file_name == ".gitignore") {
-        return false;
-    }
-
-    if (file_name.substr(file_name.size() - 3) == ".cc") {
-        return false;
-    }
-    if (file_name.substr(file_name.size() - 2) == ".c") {
-        return false;
-    }
-    if (file_name.substr(file_name.size() - 2) == ".h") {
-        return false;
-    }
-
-    return true;
-}
-
 static void traverse_directory(const fs::path& root_path) {
     stack<fs::path> stack;
     stack.push(root_path);
@@ -39,7 +17,9 @@ static void traverse_directory(const fs::path& root_path) {
                 && entry.path().filename().string()[0] != '.') {
                 stack.push(entry.path());
             } else if (entry.is_regular_file()) {
-                if (is_need_clean(entry.path().filename())) {
+                fs::file_status fileStatus = fs::status(entry.path());
+                if ((fileStatus.permissions() & fs::perms::owner_exec) != fs::perms::none) {
+                    std::cout << "delete file: " << entry.path().filename() << std::endl;
                     fs::remove(entry.path());
                     cout << entry.path().string() << endl;
                 }
