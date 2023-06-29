e1e62fee2cbe4e172ac78ce1b3b2ebbf69609d7c
gaotianpeng <gtpsydm@gmail.com>
2023-06-29 21:51:45 +0800

    daily practise

---
+++ leetcode/linklist/0369_plus_one_linked_list.cc
@@ -0,0 +1,212 @@
+#include <iostream>
+#include <random>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+/*
+    for test
+*/
+namespace {
+
+class Math {
+public:
+    static double random() {
+        static std::random_device rd;
+        static std::default_random_engine engine(rd());
+        static std::uniform_real_distribution<> distribution(0.0, 1.0);
+        return distribution(engine);
+    }
+};
+
+static void RandomArr(vector<int>& out, int max_n, int min_val, int max_val) {
+	int len = (int)(Math::random() * max_n);
+	for (int i = 0; i < len; i++) {
+		int val = (int)(Math::random() * (max_val - min_val + 1)) + min_val;
+		out.emplace_back(val);
+	}
+}
+
+struct ListNode{
+    int val  = 0;
+    ListNode* next = nullptr;
+
+    ListNode(int v):val(v) {
+    }
+};
+
+static ListNode* RandomSortedList(int max_n, int min_val, int max_val) {
+    vector<int> out;
+    RandomArr(out, max_n, min_val, max_val);
+    std::sort(out.begin(), out.end());
+    vector<ListNode*> lists;
+    for (auto& elem: out) {
+        lists.emplace_back(new ListNode(elem));
+    }
+
+    if (lists.size() == 0) {
+        return nullptr;
+    }
+
+    for (int i = 1; i < lists.size(); ++i) {
+        lists[i-1]->next = lists[i];
+    }
+    lists[lists.size() - 1]->next = nullptr;
+    return lists[0];
+}
+
+static ListNode* CopyList(ListNode* head) {
+    if (head == nullptr) {
+        return nullptr;
+    }
+
+    ListNode* ans = new ListNode(head->val);
+    ListNode* ans_head = ans;
+    while (head->next != nullptr) {
+        ans->next = new ListNode(head->next->val);
+        ans = ans->next;
+        head = head->next;
+    }
+    return ans_head;
+}
+
+static void PrintList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+static bool IsEqual(ListNode* head1, ListNode* head2) {
+    while (head1 != nullptr && head2 != nullptr) {
+        if (head1->val != head2->val) {
+            return false;
+        }
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+
+    if (head1 != nullptr || head2 != nullptr) {
+        return false;
+    }
+    return true;
+}
+
+static void FreeList(ListNode* head) {
+    while (head != nullptr) {
+        ListNode* tmp = head;
+        head = head->next;
+        delete tmp;
+    }
+}
+
+} // namespace
+
+/*
+    https://leetcode.cn/problems/7WHec2/
+    offer 077 链表排序
+        给定链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+*/
+static ListNode* reverse(ListNode* head) {
+    ListNode* pre = nullptr;
+    ListNode* next = nullptr;
+
+    while (head != nullptr) {
+        next = head->next;
+        head->next = pre;
+        pre = head;
+        head = next;
+    }
+
+    return pre;
+}
+
+static ListNode* plusOne(ListNode* head) {
+    if (head == nullptr) {
+        return nullptr;
+    }
+
+    ListNode* rev = reverse(head);
+    int carry = 1;
+    ListNode dummy(0);
+    ListNode* pre = &dummy;
+    while (rev != nullptr) {
+        int sum = rev->val + carry;
+        rev->val = sum % 10;
+        pre->next = rev;
+        pre = pre->next;
+
+        carry = sum / 10;
+        if (carry == 0) {
+            break;
+        }
+        rev = rev->next;
+    }
+
+    if (carry != 0) {
+        pre->next = new ListNode(1);
+    }
+
+    return reverse(dummy.next);
+}
+
+static ListNode* test(ListNode* head) {
+    if (head == nullptr) {
+        return head;
+    }
+
+    vector<ListNode*> nodes;
+    while (head != nullptr) {
+        nodes.emplace_back(head);
+        head = head->next;
+    }
+
+    int carry = 1;
+    int n = nodes.size();
+    for (int i = n - 1; i >= 0; i--) {
+        int sum = nodes[i]->val + carry;
+        nodes[i]->val = sum % 10;
+        carry = sum / 10;
+    }
+
+    ListNode* ans = nullptr;
+    if (carry > 0) {
+        ans = new ListNode(1);
+        ans->next = nodes[0];
+        return ans;
+    }
+
+    return nodes[0];
+}
+
+int main(int argc, char* argv[]) {
+    cout << "test start..." << endl;
+    int max = 9;
+    int min = 0;
+    int max_n = 10;
+    int test_times = 100000;
+
+    for (int i = 0; i < test_times; ++i) {
+        ListNode* head1 = RandomSortedList(max_n, min, max);
+        ListNode* head2 = CopyList(head1);
+        ListNode* ans1 = plusOne(head1);
+        ListNode* ans2 = test(head2);
+        
+        if (!IsEqual(ans1, ans2)) {
+            cout << "test failed" << endl;
+            FreeList(ans1);
+            FreeList(ans2);
+            break;
+        }
+
+        FreeList(ans1);
+        FreeList(ans2);
+    }
+
+    cout << "test end" << endl;
+
+    return 0;
+}
