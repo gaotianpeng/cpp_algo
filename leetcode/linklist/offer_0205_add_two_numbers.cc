d2d74adf8525469008c9341fe4b4d2496ef78d37
gaotianpeng <gtpsydm@gmail.com>
2023-06-29 21:09:59 +0800

    daily practise

--- leetcode/linklist/offer_0205_add_two_numbers.cc
+++ leetcode/linklist/offer_0205_add_two_numbers.cc
@@ -2,7 +2,7 @@
 #include <random>
 #include <vector>
 #include <algorithm>
-#include <queue>
+#include <deque>
 
 using namespace std;
 
@@ -29,6 +29,10 @@ static void RandomArr(vector<int>& out, int max_n, int min_val, int max_val) {
 	}
 }
 
+static int RandomVal(int min_val, int max_val) {
+    return (int)(Math::random()*(max_val - min_val)) + min_val;
+}
+
 struct ListNode{
     int val  = 0;
     ListNode* next = nullptr;
@@ -37,10 +41,9 @@ struct ListNode{
     }
 };
 
-static ListNode* RandomSortedList(int max_n, int min_val, int max_val) {
+static ListNode* RandomList(int max_n, int min_val, int max_val) {
     vector<int> out;
     RandomArr(out, max_n, min_val, max_val);
-    std::sort(out.begin(), out.end());
     vector<ListNode*> lists;
     for (auto& elem: out) {
         lists.emplace_back(new ListNode(elem));
@@ -103,122 +106,62 @@ static void FreeList(ListNode* head) {
     }
 }
 
+static int GetListLen(ListNode* head) {
+    int n = 0;
+    while (head != nullptr) {
+        ++n;
+        head = head->next;
+    }
+
+    return n;
+}
+
 } // namespace
 
 /*
-    https://leetcode.cn/problems/7WHec2/
-    offer 077 链表排序
-        给定链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+    https://leetcode.cn/problems/add-two-numbers-ii/
+    445. 两数相加 II
+    给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储一位数字。
+    将这两数相加会返回一个新的链表。
+    你可以假设除了数字 0 之外，这两个数字都不会以零开头。
 */
-static ListNode* merge(ListNode* head1, ListNode* head2) {
-    ListNode dummy(0);
-    ListNode* temp = &dummy, *temp1 = head1, *temp2 = head2;
-    while (temp1 != nullptr && temp2 != nullptr) {
-        if (temp1->val <= temp2->val) {
-            temp->next = temp1;
-            temp1 = temp1->next;
-        } else {
-            temp->next = temp2;
-            temp2 = temp2->next;
-        }
-        temp = temp->next;
-    }
 
-    if (temp1 != nullptr) {
-        temp->next = temp1;
-    } else if (temp2 != nullptr) {
-        temp->next = temp2;
-    }
-
-    return dummy.next;
+static ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    return nullptr;
 }
 
-static ListNode* sortList(ListNode* head, ListNode* tail) {
-    if (head == nullptr) {
-        return head;
-    }
-
-    if (head->next == tail) {
-        head->next = nullptr;
-        return head;
-    }
-
-    ListNode* slow = head, *fast = head;
-    while (fast != tail) {
-        slow = slow->next;
-        fast = fast->next;
-        if (fast != tail) {
-            fast = fast->next;
-        }
-    }
-
-    ListNode* mid = slow;
-    return merge(sortList(head, mid), sortList(mid, tail));
-}
-
-static ListNode* sortList(ListNode* head) {
-    return sortList(head, nullptr);
-}
-
-struct Comp {
-    bool operator() (std::pair<int, ListNode*> a, std::pair<int, ListNode*> b) {
-        return a.first > b.first;
-    }
-};
-
-static ListNode* test(ListNode* list) {
-    if (list == nullptr || list->next == nullptr) {
-        return list;
-    }
-
-    priority_queue<std::pair<int, ListNode*>, 
-        vector<std::pair<int, ListNode*>>, Comp> queue;
-
-    while (list != nullptr) {
-        queue.push(std::make_pair(list->val, list));
-        list = list->next;
-    }
-
-    ListNode* ans = queue.top().second;
-    ListNode* pre = ans;
-    queue.pop();
-
-    while (!queue.empty()) {
-        pre->next = queue.top().second;
-        queue.pop();
-        pre = pre->next;
-    }
-
-    pre->next = nullptr;
-
-    return ans;
+static ListNode* test(ListNode* l1, ListNode* l2) {
+    return nullptr;
 }
 
 int main(int argc, char* argv[]) {
     cout << "test start..." << endl;
-    int max = 100;
-    int min = -100;
-    int max_n = 10;
-    int test_times = 100000;
 
+    int max = 9;
+    int min = 0;
+    int max_n = 10;
+    int test_times = 10000;
+    
     for (int i = 0; i < test_times; ++i) {
-        ListNode* head1 = RandomSortedList(max_n, min, max);
-        ListNode* head2 = CopyList(head1);
-        ListNode* ans1 = sortList(head1);
-        ListNode* ans2 = test(head2);
-        
+        ListNode* head1 = RandomList(max_n, min, max);
+        ListNode* head2 = RandomList(max_n, min, max);
+        ListNode* ans1 = addTwoNumbers(head1, head2);
+        ListNode* ans2 = test(head1, head2);
         if (!IsEqual(ans1, ans2)) {
             cout << "test failed" << endl;
+            FreeList(head1);
+            FreeList(head2);
             FreeList(ans1);
             FreeList(ans2);
             break;
         }
 
+        FreeList(head1);
+        FreeList(head2);
         FreeList(ans1);
         FreeList(ans2);
     }
 
     cout << "test end" << endl;
-
     return 0;
 }
