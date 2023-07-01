e0b02e9ffb5f4ebc65e2fb2a3af82d0eaf8fec53
gaotianpeng <gtpsydm@gmail.com>
2023-07-01 10:46:15 +0000

    daily practise

---
+++ leetcode/array/0066_plus_one.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <random>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+
+namespace {
+class Math {
+    static double random() {
+       return 0.0; 
+    }
+};
+
+} // namespace
